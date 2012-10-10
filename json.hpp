5c871eca2ab83d500fb4579c998fc2f3f725842d
kis8P <admin.kisbox@gmail.com>
2012-10-10 15:56:15 +0400

    some work on json

--- json.hpp
+++ json.hpp
@@ -12,7 +12,7 @@ enum class ValueType {
 class Value {
 public:
     Value() : type_( ValueType::NULL_VALUE ) {}
-    Value( ValueType type ) : type_( type ) {}
+    explicit Value( ValueType type ) : type_( type ) {}
 
     ValueType getType() const { return type_; }
 
@@ -22,8 +22,8 @@ protected:
 
 class String : public Value {
 public:
-    String( std::string str ) : Value( ValueType::STRING ),
-                                data_( str ) {}
+    explicit String( std::string str ) : Value( ValueType::STRING ),
+                                         data_( str ) {}
 
     std::string get() const { return data_; }
 
@@ -32,16 +32,48 @@ private:
 };
 
 class Number : public Value {
+public:
+    explicit Number( long n ) : Value( ValueType::NUMBER ),
+                                data_( n ) {}
+
+    long get() const { return data_; }
+
+private:
+    long data_;
 };
 
 class Array : public Value {
+public:
+    Array() : Value( ValueType::ARRAY ) {}
+
+private:
+    std::vector< Value > data_;
 };
 
 class Boolean : public Value {
 };
 
 class Object : public Value {
+public:
+    struct Pair {
+        String name;
+        Value *value;
+
+        Pair( String n, Value *v ) : name( n ), value( v ) {}
+    };
+
+    Object() : Value( ValueType::OBJECT ) {}
+
+    void add( String name, Value *value ) {
+        data_.push_back( Pair( name, value ) );
+    }
+
+private:
+    std::vector< Pair > data_;
 };
+
+// Parser
+
 
 } // namespace Json
 
